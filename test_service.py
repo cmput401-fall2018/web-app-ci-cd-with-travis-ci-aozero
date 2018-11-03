632161c103444005f5d9ac2c55ebb2053a7eb925
Alexander Ozero <a.ozero@live.com>
2018-11-02 18:44:35 -0600

    Finish tests

--- test_service.py
+++ test_service.py
@@ -11,15 +11,17 @@ class TestService(unittest.TestCase):
     def test_bad_random(self, args):
         self.assertEqual(self.serv.bad_random(), 10)
 
-    # Test this
-    #def test_divide(self, y):
-    #    return self.bad_random() / y
+    @patch('service.Service.bad_random', return_value=10)
+    def test_divide(self, args):
+        self.assertEqual(self.serv.divide(5), 2)
+        self.assertEqual(self.serv.divide(-5), -2)
+        self.assertEqual(self.serv.divide(1), 10)
 
     def test_abs_plus(self):
         self.assertEqual(self.serv.abs_plus(1), 2)
         self.assertEqual(self.serv.abs_plus(0), 1)
         self.assertEqual(self.serv.abs_plus(-1), 2)
 
-    # Test this
-    #def test_complicated_function(self, x):
-    #    return self.divide(x), self.bad_random() % 2
+    @patch('service.Service.bad_random', return_value=10)
+    def test_complicated_function(self, args):
+        self.assertEqual(self.serv.complicated_function(5), (2, self.serv.bad_random()%2))
